ebdd104d3facb6127aa5ed2bc78932bd2188cf6f
gekas145 <56126336+gekas145@users.noreply.github.com>
2023-05-01 22:06:29 +0200

    first commit

---
+++ SOM.py
@@ -0,0 +1,20 @@
+
+
+class SOM:
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=none):
+        pass
+
+    def transform(self, X, y=None):
+        pass
+
+    def fit_transform(self, X, y=None):
+        pass
+
+
+
+
+
