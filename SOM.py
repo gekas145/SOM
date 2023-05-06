20066f5e3238cf16fb35438b6735d9d041237967
gekas145 <56126336+gekas145@users.noreply.github.com>
2023-05-06 17:36:40 +0200

    playing with hex coords

--- SOM.py
+++ SOM.py
@@ -1,15 +1,52 @@
+import numpy as np
 
 
 class SOM:
 
+
     def __init__(self):
         pass
 
-    def fit(self, X, y=none):
+
+    def fit(self, X, y=None):
         pass
 
-    def transform(self, X, y=None):
-        pass
+
+    @staticmethod
+    def get_n_closest_idx(X, vector, n=1):
+
+        distances = np.sqrt(np.sum((X - vector)**2, axis=-1))
+
+        distances_shape = distances.shape
+        distances = distances.flatten()
+        
+        n_closest_idx = []
+        
+
+        for i in range(n):
+
+            idx = distances.argmin()
+            distances[idx] = np.inf
+
+            n_closest_idx.append(np.unravel_index(idx, distances_shape))
+
+
+        return n_closest_idx
+
+
+
+
+if __name__ == "__main__":
+
+    som = SOM()
+
+
+    a = np.array([[[1, 3, -1], [9, 0, 2]], [[8, 4, 3], [13, 4, -2]]])
+
+    c = np.array([2, 8, 1])
+
+    print(som.get_n_closest_idx(a, c, 2))
+
 
 
 
--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
 
+col, row = 2, 3
 
-n, m = 10, 10
+n, m = 5, 5
 radius = 1
 d = np.sqrt(3)*radius/2
 margin = 0.5
 width = 2*margin + d*(2*n + 1)
 height = 2*margin + (m + m//2 + m%2)*2*d/np.sqrt(3)
+
+# axial coords
+axial = np.array([[[i, j] for j in range(n)] for i in range(m)])
+for i in range(axial.shape[0]):
+    if i % 2 == 0:
+        axial[i, :, 1] *=2
+    else:
+        axial[i, :, 1] += np.arange(1, n + 1) 
+
+dcol = np.abs(axial[:, :, 1] - axial[row, col, 1])
+drow = np.abs(axial[:, :, 0] - axial[row, col, 0])
+distances = drow + np.maximum((dcol - drow)/2, 0)
 
 fig = plt.figure()
   
@@ -22,6 +35,7 @@ for i in range(n):
     y = y = height - margin - np.sqrt(3)*d/2
     for j in range(m):
         ax.add_patch(RegularPolygon((x, y), 6, radius=radius, lw=1, edgecolor="black"))
+        ax.annotate(str(round(distances[j, i])), (x, y), weight='bold', fontsize=10*d, ha='center', va='center')
 
         if j % 2 == 0:
             x += d
